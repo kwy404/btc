e7c5a1679d7689cef008b30b7a5e0f23a1527811
kwy404 <64209066+kwy404@users.noreply.github.com>
2024-06-18 16:01:31 -0300

    Add files via upload

--- crack.py
+++ crack.py
@@ -1,4 +1,3 @@
-# EnigmaCracker
 import subprocess
 import sys
 import os
@@ -76,33 +75,17 @@ def update_cmd_title():
         os.system(f"title EnigmaCracker.py - Wallets Scanned: {wallets_scanned}")
 
 
-def bip():
+def generate_seed_and_private_key():
     # Generate a 12-word BIP39 mnemonic
-    return Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
-
-
-def bip44_ETH_wallet_from_seed(seed):
-    # Generate an Ethereum wallet from a BIP39 seed.
-
+    mnemonic = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
+    
     # Generate the seed from the mnemonic
-    seed_bytes = Bip39SeedGenerator(seed).Generate()
+    seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
 
-    # Create a Bip44 object for Ethereum derivation
-    bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.ETHEREUM)
+    # Derive the private key for Bitcoin
+    btc_priv_key = Bip44.FromSeed(seed_bytes, Bip44Coins.BITCOIN).PrivateKey().Raw().ToHex()
 
-    # Derive the account 0, change 0, address_index 0 path (m/44'/60'/0'/0/0)
-    bip44_acc_ctx = (
-        bip44_mst_ctx.Purpose()
-        .Coin()
-        .Account(0)
-        .Change(Bip44Changes.CHAIN_EXT)
-        .AddressIndex(0)
-    )
-
-    # Get the Ethereum address
-    eth_address = bip44_acc_ctx.PublicKey().ToAddress()
-
-    return eth_address
+    return mnemonic, btc_priv_key
 
 
 def bip44_BTC_seed_to_address(seed):
@@ -119,35 +102,6 @@ def bip44_BTC_seed_to_address(seed):
 
     # Print the address
     return bip44_addr_ctx.PublicKey().ToAddress()
-
-
-def check_ETH_balance(address, etherscan_api_key, retries=3, delay=5):
-    # Etherscan API endpoint to check the balance of an address
-    api_url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={etherscan_api_key}"
-
-    for attempt in range(retries):
-        try:
-            # Make a request to the Etherscan API
-            response = requests.get(api_url)
-            data = response.json()
-
-            # Check if the request was successful
-            if data["status"] == "1":
-                # Convert Wei to Ether (1 Ether = 10^18 Wei)
-                balance = int(data["result"]) / 1e18
-                return balance
-            else:
-                logging.error("Error getting balance: %s", data["message"])
-                return 0
-        except Exception as e:
-            if attempt < retries - 1:
-                logging.error(
-                    f"Error checking balance, retrying in {delay} seconds: {str(e)}"
-                )
-                time.sleep(delay)
-            else:
-                logging.error("Error checking balance: %s", str(e))
-                return 0
 
 
 def check_BTC_balance(address, retries=3, delay=5):
@@ -169,10 +123,10 @@ def check_BTC_balance(address, retries=3, delay=5):
                 return 0
 
 
-def write_to_file(seed, BTC_address, BTC_balance, ETH_address, ETH_balance):
+def write_to_file(seed, BTC_address, BTC_balance):
     # Write the seed, address, and balance to a file in the script's directory
     with open(wallets_file_path, "a") as f:
-        log_message = f"Seed: {seed}\nAddress: {BTC_address}\nBalance: {BTC_balance} BTC\n\nEthereum Address: {ETH_address}\nBalance: {ETH_balance} ETH\n\n"
+        log_message = f"Seed: {seed}\nAddress: {BTC_address}\nBalance: {BTC_balance} BTC\n\n"
         f.write(log_message)
         logging.info(f"Written to file: {log_message}")
 
@@ -181,37 +135,22 @@ def main():
     global wallets_scanned
     try:
         while True:
-            seed = bip()
-            # BTC
+            seed, btc_private_key = generate_seed_and_private_key()
             BTC_address = bip44_BTC_seed_to_address(seed)
             BTC_balance = check_BTC_balance(BTC_address)
 
             logging.info(f"Seed: {seed}")
             logging.info(f"BTC address: {BTC_address}")
             logging.info(f"BTC balance: {BTC_balance} BTC")
+            logging.info(f"BTC address private: {btc_private_key}")
             logging.info("")
 
-            # ETH
-            ETH_address = bip44_ETH_wallet_from_seed(seed)
-            ###!
-            etherscan_api_key = os.getenv("ETHERSCAN_API_KEY")
-            if not etherscan_api_key:
-                raise ValueError(
-                    "The Etherscan API key must be set in the environment variables."
-                )
-            ###!
-            ETH_balance = check_ETH_balance(ETH_address, etherscan_api_key)
-            logging.info(f"ETH address: {ETH_address}")
-            logging.info(f"ETH balance: {ETH_balance} ETH")
-
-            # Increment the counter and update the CMD title
             wallets_scanned += 1
             update_cmd_title()
 
-            # Check if the address has a balance
-            if BTC_balance > 0 or ETH_balance > 0:
+            if BTC_balance > 0:
                 logging.info("(!) Wallet with balance found!")
-                write_to_file(seed, BTC_address, BTC_balance, ETH_address, ETH_balance)
+                write_to_file(seed, BTC_address, BTC_balance)
 
     except KeyboardInterrupt:
         logging.info("Program interrupted by user. Exiting...")
